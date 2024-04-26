189d28ba6056d9878de9e84b963e41ca0b40f8f0
Bayron David <93720978+BayronDavid@users.noreply.github.com>
2024-04-26 11:22:29 -0500

    Implement, OSD Annotation

--- js/events.js
+++ js/events.js
@@ -1,7 +1,7 @@
 let viewer = null;
 
 document.addEventListener("DOMContentLoaded", async () => {
-  const imageList = document.getElementById("image-list");
+  const imageList = document.getElementById("imageList");
   const uploadForm = document.getElementById("upload-form");
 
   const loadingModal = new bootstrap.Modal(
@@ -13,55 +13,51 @@ document.addEventListener("DOMContentLoaded", async () => {
   );
 
   // Función para cargar la lista de imágeness
-   const loadImages = async () => {
-     try {
-       const response = await axios.get(
-         "http://127.0.0.1:8000/api/list_images"
-       );
-       const images = response.data;
-       const imageList = document.getElementById("imageList");
-       imageList.innerHTML = "";
+  const loadImages = async () => {
+    try {
+      const response = await axios.get("http://127.0.0.1:8000/api/list_images");
+      const images = response.data;
+      imageList.innerHTML = "";
 
-       images.forEach((image) => {
-         const listItem = document.createElement("div");
-         listItem.className = "image-container list-group-item";
+      images.forEach((image) => {
+        const listItem = document.createElement("div");
+        listItem.className = "image-container list-group-item";
 
-         // Contenido del contenedor de la imagen
-         listItem.innerHTML = `
+        // Contenido del contenedor de la imagen
+        listItem.innerHTML = `
           <img src="${image.thumbnail_url}" alt="${image.filename}" class="img-thumbnail">
           <div class="image-details">${image.filename}</div>
           <button class="btn btn-danger btn-sm delete-button" data-image-id="${image.id}"><i class="fa-solid fa-trash"></i></button>
         `;
 
-         // Agregar evento de clic para cargar la imagen en el visor
-         listItem.querySelector("img").addEventListener("click", () => {
-           showImage(image.id);
-         });
+        // Agregar evento de clic para cargar la imagen en el visor
+        listItem.querySelector("img").addEventListener("click", () => {
+          showImage(image.id);
+        });
 
-         // Agregar evento de clic para eliminar la imagen
-         listItem
-           .querySelector(".delete-button")
-           .addEventListener("click", async (event) => {
-             const imageId = event.target.getAttribute("data-image-id");
-             await deleteImage(imageId);
-             // Recargar la lista de imágenes después de eliminar
-             loadImages();
-           });
+        // Agregar evento de clic para eliminar la imagen
+        listItem
+          .querySelector(".delete-button")
+          .addEventListener("click", async (event) => {
+            const imageId = event.target.getAttribute("data-image-id");
+            await deleteImage(imageId);
+            // Recargar la lista de imágenes después de eliminar
+            loadImages();
+          });
 
-         imageList.appendChild(listItem);
-       });
+        imageList.appendChild(listItem);
+      });
 
-       loadingModal.hide();
-       // Mostrar la primera imagen en el visor por defecto
-       if (images.length > 0) {
-         showImage(images[0].id);
-       }
-     } catch (error) {
-       console.error("Error al cargar la lista de imágenes:", error);
-     }
-   };
+      loadingModal.hide();
+      // Mostrar la primera imagen en el visor por defecto
+      if (images.length > 0) {
+        showImage(images[0].id);
+      }
+    } catch (error) {
+      console.error("Error al cargar la lista de imágenes:", error);
+    }
+  };
 
-  // Función para cargar una imagen en el visor
   const showImage = async (imageId) => {
     logMessage(`Cargando imagen ${imageId}...`);
     try {
@@ -72,27 +68,71 @@ document.addEventListener("DOMContentLoaded", async () => {
       const tilesBaseUrl = `http://127.0.0.1:8000/dzi/${imageId}_files/`;
       const tileSource = tileSourceFromData(dziData, tilesBaseUrl);
 
-      // Destruye el visor actual si ya existe
       if (viewer) {
         viewer.destroy();
-        viewer = null;
       }
 
-      // Crea un nuevo visor
       viewer = OpenSeadragon({
         id: "openseadragon1",
         prefixUrl: "//openseadragon.github.io/openseadragon/images/",
         tileSources: tileSource,
-        zoomInButton: "zoom-in",
-        zoomOutButton: "zoom-out",
-        homeButton: "home",
-        fullPageButton: "full-page",
-        nextButton: "next",
-        previousButton: "previous",
       });
 
+      const anno = OpenSeadragon.Annotorious(viewer);
+
+      // Cargar anotaciones
+      anno.loadAnnotations("http://127.0.0.1:8000/api/annotations/" + imageId);
+
+      anno.on("createAnnotation", async (annotation) => {
+        logMessage("Creando anotación...");
+        try {
+          await axios.post(
+            `http://127.0.0.1:8000/api/annotations/${imageId}`,
+            annotation
+          );
+          logMessage("Anotación añadida con éxito.");
+        } catch (error) {
+          console.error("Error al guardar la anotación:", error);
+          logMessage("Error al guardar la anotación.");
+        }
+      });
+
+      anno.on("updateAnnotation", async (annotation, previous) => {
+        // console.log(
+        //   `http://127.0.0.1:8000/api/annotations/${imageId}/${previous.id}`,
+        //   annotation
+        // );
+        logMessage("Actualizando anotación...");
+        const encodedAnnotationId = encodeURIComponent(previous.id);
+        try {
+          await axios.put(
+            `http://127.0.0.1:8000/api/annotations/${imageId}/${encodedAnnotationId}`,
+            annotation
+          );
+          logMessage("Anotación actualizada con éxito.");
+        } catch (error) {
+          console.error("Error al actualizar la anotación:", error);
+          logMessage("Error al actualizar la anotación.");
+        }
+      });
+
+      anno.on("deleteAnnotation", async (annotation) => {
+        logMessage("Eliminando anotación...");
+        const encodedAnnotationId = encodeURIComponent(annotation.id);
+        try {
+          await axios.delete(
+            `http://127.0.0.1:8000/api/annotations/${imageId}/${encodedAnnotationId}`
+          );
+          logMessage("Anotación eliminada con éxito.");
+        } catch (error) {
+          console.error("Error al eliminar la anotación:", error);
+          logMessage("Error al eliminar la anotación.");
+        }
+      });
+
+
       viewer.addHandler("tile-loaded", function (event) {
-        logMessage(` ${event.tile.getUrl()}`);
+        logMessage(`Tile cargado: ${event.tile.getUrl()}`);
       });
     } catch (error) {
       console.error(`Error al cargar la imagen ${imageId}:`, error);
@@ -100,7 +140,8 @@ document.addEventListener("DOMContentLoaded", async () => {
     }
   };
 
-  // Función para eliminar una imagen
+
+  
   const deleteImage = async (imageId) => {
     try {
       const response = await axios.delete(
